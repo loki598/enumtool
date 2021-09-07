c7fffbdae3b5f76d0a4c401179667c97a2314c10
Legend <73299970+loki598@users.noreply.github.com>
2021-09-07 03:48:13 -0400

    Add files via upload

---
+++ enumtool.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#simple tool to make your enum easy
+echo "input the ip " "if you want to scan a https website please wait till the tool is fully developed";
+
+read  ip
+
+if [$ip=""]
+then
+ echo usage =192.168.1.1 "if you want to scan a https website please wait till the tool is fully developed";
+
+else
+
+dirb http://$ip | grep http
+
+nikto -host http://$ip |  grep -v Nikto
+
+
+
+nmap -sV --script=http-vuln-* $ip | grep -v Nmap | grep -v Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
+
+
+fi
+
